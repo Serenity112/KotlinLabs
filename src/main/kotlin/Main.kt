2c688180ba5cc3d30d6f58c6162636558378c625
Serenity112 <skuratov.prodamgarazh@gmail.com>
2022-09-16 00:34:31 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+import lab1.WikipediaSearch
+
+fun main() {
+    println("Enter search:")
+    val userSearch = readln()
+
+    try {
+        val searchResult = WikipediaSearch(userSearch)
+
+        searchResult.wikipediaResults.forEach {
+            println(it)
+        }
+
+        println("Choose page ID to open")
+        val id = readln().toInt()
+
+        searchResult.openLink(id)
+    } catch (e: java.lang.Exception) {
+        println(e.message)
+    } catch (e: java.lang.NumberFormatException) {
+        println("Enter a number!")
+    } catch (e: java.lang.IndexOutOfBoundsException) {
+        println("Enter a valid page ID!")
+    }
+}
